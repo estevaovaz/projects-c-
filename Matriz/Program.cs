8bbd3115a3b4a1ff1fa5ed0127771e3ebf29fd26
Estevao Vaz <estevaobarreto8@gmail.com>
2019-05-03 21:38:13 -0300

    Projeto De Exercicio de Uma Matriz

---
+++ Matriz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region EXERCICIO DE MATRIZ
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            //linhas
+            for (int i = 0; i < n; i++)
+            {
+                string[] linha = Console.ReadLine().Split(' ');
+                //colunas
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(linha[j]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.Write("Negative numbers: " + count);
+            #endregion
+
+        }
+    }
+}
